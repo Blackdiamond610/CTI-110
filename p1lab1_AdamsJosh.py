cc51ecf561faa322dba3358c7e795ebfc73925b1
Blackdiamond610 <146122296+Blackdiamond610@users.noreply.github.com>
2023-09-26 13:50:20 -0400

    Add files via upload

---
+++ p1lab1_AdamsJosh.py
@@ -0,0 +1,5 @@
+#Josh Adams
+#9/26/23
+#Write a program that takes first name as an input and output a welcome message with that name
+first_name = input("Please Enter Your Name:")
+print("Hello",first_name, "and welcome to CS Online!")
