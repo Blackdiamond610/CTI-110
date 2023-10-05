2e12db3ba0323dc88e4449e95bae6d19886281ed
Blackdiamond610 <146122296+Blackdiamond610@users.noreply.github.com>
2023-10-05 09:20:24 -0400

    Add files via upload

---
+++ P1HW1_AdamsJosh.py
@@ -0,0 +1,12 @@
+#Josh Adams
+#10/5/2023
+#Basic Outputs
+int_num=int(input("Enter integer: "))
+print(int_num)
+print("You entered:",int_num)
+print(int_num,"squared is",int_num**2)
+print("and",int_num,"cubed is",int_num**3,"!!!")
+int_num2=int(input("Enter another integer"))
+print(int_num2)
+print(int_num + int_num2)
+print(int_num * int_num2)
